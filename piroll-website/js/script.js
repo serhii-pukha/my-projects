2b868ede049ad3d0a9ad7c1f63a169ec470d95e8
s-pukha <68913393+s-pukha@users.noreply.github.com>
2021-12-05 17:19:48 +0100

    Add files via upload

---
+++ piroll-website/js/script.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").classList.add("header--light");
+  } else {
+    document.getElementById("header").classList.remove("header--light");
+  }
+}
+
+function toggleMenu() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
