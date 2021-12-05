345c75613ac981c716daf16e0a6fea9ef4e93210
s-pukha <68913393+s-pukha@users.noreply.github.com>
2021-12-05 17:11:32 +0100

    Add files via upload

---
+++ fashion-news-portal-website/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("openMenu").addEventListener('click', function(){
+  document.getElementById("myNav").style.width = "100%";
+})
+
+document.getElementById("closeMenu").addEventListener('click', function(){
+  document.getElementById("myNav").style.width = "0%";
+})
